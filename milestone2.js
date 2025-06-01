7f7e6e6bc2962b24a9b99daa50c9f9d89205b688
sakamw <shanmwangi2020@gmail.com>
2025-06-01 16:29:56 +0300

    Completed Challenge 2: Find Maximum Value

--- milestone2.js
+++ milestone2.js
@@ -9,3 +9,14 @@ var sumOfPositives = function (numbers) {
     return sum;
 };
 console.log(sumOfPositives([1, -3, 5, -2, 9, -8]));
+// Find Maximum Value
+var findMax = function (numbers) {
+    var max = numbers[0];
+    for (var _i = 0, numbers_2 = numbers; _i < numbers_2.length; _i++) {
+        var num = numbers_2[_i];
+        if (num > max)
+            max = num;
+    }
+    return max;
+};
+console.log(findMax([3, 7, 2, 9, 5]));
--- milestone2.ts
+++ milestone2.ts
@@ -7,3 +7,13 @@ const sumOfPositives = (numbers: number[]): number => {
   return sum;
 };
 console.log(sumOfPositives([1, -3, 5, -2, 9, -8]));
+
+// Find Maximum Value
+const findMax = (numbers: number[]): number => {
+  let max = numbers[0];
+  for (const num of numbers) {
+    if (num > max) max = num;
+  }
+  return max;
+};
+console.log(findMax([3, 7, 2, 9, 5]));
